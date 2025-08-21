9610522eaed99cff1a358448dffb8dd026fa8e2c
zhangdx <zdx3987@163.com>
2025-08-21 22:43:15 +0800

    fix:增加mybatis的一些代码

--- improve-mybatis-spring/src/main/java/cn/zhangdx/mybatis/config/MybatisConfig.java
+++ improve-mybatis-spring/src/main/java/cn/zhangdx/mybatis/config/MybatisConfig.java
@@ -44,11 +44,11 @@ public class MybatisConfig {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource());
         sqlSessionFactoryBean.setConfigLocation(resourceLoader.getResource("classpath:mybatis-config.xml"));
-        try {
-            sqlSessionFactoryBean.setMapperLocations(resourceLoader.getResources("classpath:mapper/*.xml"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            sqlSessionFactoryBean.setMapperLocations(resourceLoader.getResources("classpath:mapper/*.xml"));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
         return sqlSessionFactoryBean;
     }
 }
