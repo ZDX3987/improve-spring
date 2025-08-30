76de30d29e00cdb2901ba079bfa76fbbe58223e0
zhangdx <zdx3987@163.com>
2025-08-30 14:41:11 +0800

    fix:提交改动代码

--- improve-mybatis-spring/src/main/java/cn/zhangdx/mybatis/ImproveMybatisSpringApp.java
+++ improve-mybatis-spring/src/main/java/cn/zhangdx/mybatis/ImproveMybatisSpringApp.java
@@ -18,16 +18,20 @@ import java.util.Set;
 public class ImproveMybatisSpringApp {
     public static void main( String[] args ) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MybatisConfig.class);
-        // Mybatis原始方式
-        SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) applicationContext.getBean("sqlSessionFactoryBean");
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        List<SysUser> userList = sqlSession.selectList("cn.zhangdx.mybatis.mapper.SysUserMapper.findUserNames", "ZH");
-        userList.forEach(user -> System.out.println(user.getId() + ":" + user.getUsername()));
-        sqlSession.close();
-
-        // Spring Bean方式
-        SysUserService sysUserService = applicationContext.getBean(SysUserService.class);
-        userList = sysUserService.getUserList("ZH");
-        userList.forEach(user -> System.out.println(user.getId() + ":" + user.getUsername()));
+        boolean useSpringType = true;
+        List<SysUser> userList;
+        if (!useSpringType) {
+            // Mybatis原始方式
+            SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) applicationContext.getBean("sqlSessionFactoryBean");
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            userList = sqlSession.selectList("cn.zhangdx.mybatis.mapper.SysUserMapper.findUserNames", "ZH");
+            userList.forEach(user -> System.out.println(user.getId() + ":" + user.getUsername()));
+            sqlSession.close();
+        } else {
+            // Spring Bean方式
+            SysUserService sysUserService = applicationContext.getBean(SysUserService.class);
+            userList = sysUserService.getUserList("ZH");
+            userList.forEach(user -> System.out.println(user.getId() + ":" + user.getUsername()));
+        }
     }
 }
